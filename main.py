014646790f931f1dbbb6b4aa2dc5d81ff73cf4fa
Bhushan <bhusahn88@gmail.com>
2017-09-25 16:24:24 -0500

    Finish webcaesar

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from caesar import rotate_string
+
 app=Flask(__name__)
 app.config['DEBUG']= True
+
 # a form for caeser encryption
 form = """
 <!DOCTYPE html>
@@ -9,32 +11,32 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <!-- create your form here -->
     
-      <form action="/" method="post">
+      <form method="post">
         
         <label for="rot">
         Rotate by: 
             </label>
             <input type="text" id="rot" name="rot" value = "0"/>
            
-            <textarea type="text" name="text"></textarea>
+            <textarea name= "text">{content}</textarea>
             <input type="submit" value="Submit Query" />
             
           </form>
@@ -43,20 +45,21 @@ form = """
 """
 #create a route above the function definition to receive and handle the request form 
 
+
+
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = request.form['text'] 
-    content = rotate_string(text, rot)
-    sentence = "<h1>"  +content + "</h1>"
-    return sentence
+    encrypted = rotate_string(text, rot)
+    
+    return form.format(content=encrypted)
 
 
 
 
 @app.route("/")
 def index():
-    return form
-
+    return form.format(content="")
     
 app.run()
